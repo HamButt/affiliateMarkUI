1308e75d102320aa2e089b6eaf29296fe61380fe
abc <MY_NAME@example.com>
2024-11-02 00:21:18 +0500

    padding added

--- src/components/Section.js
+++ src/components/Section.js
@@ -13,24 +13,24 @@ function Section({data}) {
 
   return (
 
-    <motion.div  className='min-h-screen h-full py-8 px-4 md:px-5'>
-        <div className="flex items-center justify-between">
+    <motion.div  className='min-h-screen h-full py-8 mt-12 px-4 md:px-5'>
+        <div className="flex items-center justify-between bg-gray-300 p-2 rounded">
             <h1 className='text-xl md:text-4xl text-black font-semibold'>Baby products</h1>
             <h1 className='text-lg md:text-4xl text-black font-semibold'>BIG SALE  <sup >60% OFF</sup></h1>
         </div>
         
-        <section className='flex flex-wrap justify-center sm:justify-between mt-5' >
+        <section className='flex flex-wrap justify-center sm:justify-between mt-2' >
 
             {     
               data?.map((d) => {
                   return (
                   <div key={d.id} className="sm:w-[310px] mt-3 rounded overflow-hidden shadow-lg bg-white dark:bg-white transform transition-transform duration-300">
-                        
-                        {d.src ? <Image src={d.src} imageClassName="rounded-t w-full"  alt="Image" preview /> : 
+                  <Image src={d.src} imageClassName="rounded-t w-full"  alt="Image" preview />
+                        {/* {d.src ? : 
                         <div className='w-full text-center flex items-center justify-center py-20' >
                           <span className='loading loading-md' ></span>
                         </div>
-                        }
+                        } */}
                         <div className="px-3 py-2">
 
                         <h3 className="text-md font-semibold text-black dark:text-black line-clamp-1 ">{d?.heading}</h3>
